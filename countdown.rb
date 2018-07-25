891884365e6a3c3f7b4dade2d527d1e73faeaeb9
github username <anthony.s.lee96@gmail.com>
2018-07-25 15:41:17 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  count = 0
+  while count < integer
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    break if count == integer
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+  sleep 5
 end
